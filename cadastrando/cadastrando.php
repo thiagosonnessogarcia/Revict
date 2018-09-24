d2910e2340f08a6df35530bbaefdfa4d595840f4
Thiago Garcia <thiagosonnesso@hotmail.com>
2018-09-24 14:40:31 -0300

    Atualizando campos (sepração de códigos)

--- cadastrando/cadastrando.php
+++ cadastrando/cadastrando.php
@@ -14,10 +14,8 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = addslashes(sha1($_POST['senha']));
 $cpf = $_POST['cpf'];
-$valordivida = $_POST['valordivida'];
-$saldo = $_POST['saldo'];
 
-$sql = mysql_query("INSERT INTO usuario(nome, email, senha, cpf, valordivida, saldo) VALUES ('$nome', '$email', '$senha', '$cpf', '$valordivida', '$saldo')");
+$sql = mysql_query("INSERT INTO usuario(nome, email, senha, cpf) VALUES ('$nome', '$email', '$senha', '$cpf')");
 
 echo "<center><h2>Cadastrado com Sucesso!!!</h2></center>";
 header("Location: login.php");
