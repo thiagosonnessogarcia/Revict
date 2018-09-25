10d3d5148417db71b202ad72cdcef2335e5c1370
Thiago Garcia <thiagosonnesso@hotmail.com>
2018-09-25 14:01:02 -0300

    BUG - ATAULIZANDO TODOS OS ARQUIVOS

--- Crud/select.php
+++
@@ -1,8 +0,0 @@
-<?php
-$consulta = $pdo- >query("SELECT nome FROM usuario;");
- 
-  
-while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
-    echo "Nome: {$linha['nome']} - Usuário: {$linha['usuario']}<br />";
-}
-?>
--- HomePage/cad_dividas/deletar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$sql = " DELETE FROM dividas WHERE (id_cliente, nomegasto, divida)";
-
-echo "<b>nomegasto</b> foi deletado com êxito!<BR>";
-
-echo "<a href=atualizar.php><b>Lista Atualizada</b></a> | ";
-
-?>
