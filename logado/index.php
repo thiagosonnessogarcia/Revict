a7cf6b4264ff0636c95cd28bf9581146db6525ab
Thiago Garcia <thiagosonnesso@hotmail.com>
2018-09-26 16:25:58 -0300

    ATUALIZAÇÃO / CRUD FUNCIONANDO

--- logado/index.php
+++ logado/index.php
@@ -1,16 +1,5 @@
 <?php 
-
-session_start();
-if((!isset ($_SESSION['email']) == true) and (!isset ($_SESSION['senha']) == true))
-{
-  unset($_SESSION['email']);
-  unset($_SESSION['senha']);
-  header('location:index.php');
-  }
- 
-$logado = $_SESSION['email'];
-?>
-
+  include_once("conexaouser.php");
 ?>
 
 <!DOCTYPE html>
